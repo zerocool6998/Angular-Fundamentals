1283a4d52e864fd65a3b963dffcfcc8efdf4aa13
zerocool <anas.nouar@usms.ac.ma>
2023-11-02 19:35:25 +0100

    Angular Observables

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,28 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent  {
+export class AppComponent implements OnInit  {
   name= 'Angular';
-  ids: Array<string> = ['one', 'two', 'three', 'four'];
+  // ids: Array<string> = ['one', 'two', 'three', 'four'];
+
+  observable = new Observable((observer) => {
+    observer.next("Start Processing ...");
+    setTimeout(() => observer.next("Still Processing"), 3000);
+    setTimeout(() => observer.complete(), 5000);
+  })
+
+  
+  ngOnInit(){
+    this.observable.subscribe({
+      next: (data) => console.log(data),
+      error: (err) => console.error(err),
+      complete: () => console.log("Process Complete")
+}
+    )     
+  }
 }
